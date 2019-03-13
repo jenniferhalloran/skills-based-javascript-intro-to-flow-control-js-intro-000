5943860c5b7e44789447754310069ea0c3f695d9
Jennifer Halloran <jenniferlhalloran@gmail.com>
2019-03-13 20:45:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,11 +4,12 @@ return "You are a teenager!";
 }
 }
 
+
 function teenager(age) {
 if (age>=13 && age<=19) {
 return "You are a teenager!";
 } else {
-  return "You are not a teenager";
+return "You are not a teenager";
 }
 }
 
